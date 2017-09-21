e4d00813df03ccb2c47d019a8a2a0ec2c034db61
William Swanson <swansontec@gmail.com>
2017-09-21 11:26:51 -0700

    Update typings

--- redux-keto.d.ts
+++ redux-keto.d.ts
@@ -1,23 +1,23 @@
-export interface Action {
-  type: any
-  [extraProps: string]: any
-}
-
-export interface Peers {
-  unchanged: boolean
-  [peers: string]: any
-}
-
-export type Reducer<S> = (
-  state: S | undefined,
-  action: Action,
-  peers?: Peers
+export type FatReducer<S, A, P> = (
+  state: S | void,
+  action: A,
+  props: P,
+  oldProps: P
 ) => S
 
-/**
- * Combines several reducers into one.
- */
-export function buildReducer<S> (
-  reducerMap: { [key: string]: Reducer<any> },
-  filterPeers?: (peers: Peers) => {}
-): Reducer<S>
+export function buildReducer<A, P> (reducerMap: {
+  [key: string]: FatReducer<any, A, P>
+}): FatReducer<{ [key: string]: any }, A, P>
+
+export function wrapReducer<S, A, P, Ai, Pi> (
+  reducer: FatReducer<S, Ai, Pi>,
+  filterProps?: (props: P) => Pi,
+  filterAction?: (action: A) => Ai
+): FatReducer<S, A, P>
+
+export function repeatReducer<S, A, P, Ai, Pi> (
+  reducer: FatReducer<S, Ai, Pi>,
+  listIds: (props: P) => Array<string>,
+  filterProps?: (props: P, id: string) => Pi,
+  filterAction?: (action: A, id: string) => Ai
+): FatReducer<{ [key: string]: S }, A, P>
