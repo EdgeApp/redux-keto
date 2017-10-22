a5b694a6e5846053ab2981dd6879d20d955098aa
William Swanson <swansontec@gmail.com>
2017-10-22 04:20:50 -0700

    Fix a build warning when running tests

--- rollup.config.test.js
+++ rollup.config.test.js
@@ -1,7 +1,8 @@
 import config from './rollup.config.js'
+import packageJson from './package.json'
 
 export default {
-  external: ['chai', 'redux'],
+  external: Object.keys(packageJson.devDependencies),
   input: 'test/tests.js',
   output: [{ file: 'build/tests.js', format: 'cjs' }],
   plugins: config.plugins,
